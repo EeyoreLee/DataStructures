998bf85416c6b50063abb984aeb938ee68416092
EeyoreLee <li.chunyu0412@gmail.com>
2023-09-08 17:40:52 +0800

    update

---
+++ course/0201_union/SqList.h
@@ -0,0 +1,31 @@
+#ifndef SQLIST_H
+#define SQLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Status.h>
+
+#define LIST_INIT_SIZE 100
+#define LISTINCREMENT 10
+
+typedef int ElemType;
+
+typedef struct SqList
+{
+    ElemType *elem;
+    int length;
+    int listsize;
+} SqList;
+
+Status InitList(SqList *L);
+
+int ListLength(SqList L);
+
+Status GetElem(SqList L, int i, ElemType *e);
+
+int LocateElem(SqList L, ElemType e, Status(Compare)(ElemType, ElemType));
+
+Status ListInsert(SqList *L, int i, ElemType e);
+
+void ListTraverse(SqList L, void(Visit)(ElemType));
+#endif
